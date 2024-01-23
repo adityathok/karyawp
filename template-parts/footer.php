b4054e7798a04399ab73480ee05167332e5cc23d
adityathok <aditya0thok@gmail.com>
2024-01-23 20:19:12 +0700

    footer part

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -9,6 +9,26 @@
 defined( 'ABSPATH' ) || exit;
 
 ?>
-<footer id="site-footer" class="site-footer" role="contentinfo">
+<footer id="site-footer" class="site-footer py-4 py-md-5 mt-5 bg-body-tertiary" role="contentinfo">
+
+    <div class="container">
+        <div class="site-info">
+                <?php
+                printf('© %s <a href="%s"> %s </a>. All rights reserved.', 
+                    date('Y'),
+                    home_url( '/' ),
+                    get_bloginfo( 'name') 
+                );
+                ?>
+                
+                <span class="sep"> | </span>
+
+                <?php
+                /* translators: 1: Theme name, 2: Theme author. */
+                printf( esc_html__( 'Theme: %1$s by %2$s.', 'zahro' ), 'Zahro', '<a href="https://github.com/adityathok/zahro/">Adityathok</a>' );
+                ?>
+
+        </div><!-- .site-info -->
+    </div>
 
 </footer>
