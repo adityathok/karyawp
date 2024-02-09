cb320680ae96f3494fad534c400624d9fc77ccf1
adityathok <aditya0thok@gmail.com>
2024-02-09 13:57:47 +0700

    update karyawp_entry_meta()

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -53,13 +53,11 @@ if ( ! function_exists( 'karyawp_entry_meta' ) ) {
 	 * Prints HTML with meta information for the current post-date/time and author.
 	 */
 	function karyawp_entry_meta() {
-		$time_string = '<time class="entry-date published updated" datetime="%1$s"><svg xmlns="http://www.w3.org/2000/svg" style="vertical-align: inherit;" width="11" height="11" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16"> <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/> <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/> </svg> %2$s</time>';
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		$time_string = sprintf(
 			$time_string,
 			esc_attr( get_the_date( 'c' ) ),
-			esc_html( get_the_date() ),
-			esc_attr( get_the_modified_date( 'c' ) ),
-			esc_html( get_the_modified_date() )
+			esc_html( get_the_date() )
 		);
 
 		$post_date = apply_filters(
@@ -72,8 +70,7 @@ if ( ! function_exists( 'karyawp_entry_meta' ) ) {
 		$post_author = apply_filters(
 			'karyawp_entry_meta_author',
 			sprintf(
-				'<span class="post-meta-author"> %1$s<span class="author vcard"> <a class="url fn n" href="%2$s">%3$s</a></span></span>',
-				'<svg xmlns="http://www.w3.org/2000/svg" style="vertical-align: inherit;" width="11" height="11" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16"> <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/> <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/> </svg>',
+				'<span class="post-meta-author"><span class="author vcard"><a class="url fn n" href="%1$s">%2$s</a></span></span>',
 				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
 				esc_html( get_the_author() )
 			)
@@ -84,10 +81,10 @@ if ( ! function_exists( 'karyawp_entry_meta' ) ) {
 		if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 			$post_comments = '<span class="post-meta-comments">';
 			$post_comments .= sprintf(
-				'%1$s <a class="url fn n" href="%2$s">%3$s</a>',
-				'<svg xmlns="http://www.w3.org/2000/svg" style="vertical-align: inherit;" width="11" height="11" fill="currentColor" class="bi bi-chat-left" viewBox="0 0 16 16"> <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/> </svg>',
+				'<a class="url fn n" href="%1$s">%2$s %3$s</a>',
 				esc_url( get_the_permalink().'#comments' ),
-				get_comments_number()
+				get_comments_number(),
+				get_comments_number() == 1 ? esc_html__( 'Comment', 'karyawp' ) : esc_html__( 'Comments', 'karyawp' )
 			);
 			$post_comments .= '</span>';
 
@@ -101,12 +98,13 @@ if ( ! function_exists( 'karyawp_entry_meta' ) ) {
 				'<span class="post-meta-separator px-2">·</span>',
 			)
 		);
+		
+		$post_meta = apply_filters('karyawp_entry_meta',$post_meta);
 
 		echo '<span class="karyawp_entry_meta">'.implode($post_meta_separator,$post_meta).'</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 	}
 }
-
 
 if ( ! function_exists( 'karyawp_comment_navigation' ) ) {
 	/**
@@ -122,17 +120,17 @@ if ( ! function_exists( 'karyawp_comment_navigation' ) ) {
 		?>
 		<nav class="comment-navigation" id="<?php echo esc_attr( $nav_id ); ?>">
 
-			<h1 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'wpzaro' ); ?></h1>
+			<h1 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'karyawp' ); ?></h1>
 
 			<?php if ( get_previous_comments_link() ) { ?>
 				<div class="nav-previous">
-					<?php previous_comments_link( __( '&larr; Older Comments', 'wpzaro' ) ); ?>
+					<?php previous_comments_link( __( '&larr; Older Comments', 'karyawp' ) ); ?>
 				</div>
 			<?php } ?>
 
 			<?php if ( get_next_comments_link() ) { ?>
 				<div class="nav-next">
-					<?php next_comments_link( __( 'Newer Comments &rarr;', 'wpzaro' ) ); ?>
+					<?php next_comments_link( __( 'Newer Comments &rarr;', 'karyawp' ) ); ?>
 				</div>
 			<?php } ?>
 
@@ -149,7 +147,7 @@ if ( ! function_exists( 'karyawp_edit_post_link' ) ) {
 		edit_post_link(
 			sprintf(
 				/* translators: %s: Name of current post */
-				esc_html__( 'Edit %s', 'wpzaro' ),
+				esc_html__( 'Edit %s', 'karyawp' ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			),
 			'<span class="edit-link">',
@@ -171,14 +169,14 @@ if ( ! function_exists( 'karyawp_post_nav' ) ) {
 		}
 		?>
 		<nav class="container navigation post-navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Post navigation', 'wpzaro' ); ?></h2>
+			<h2 class="screen-reader-text"><?php esc_html_e( 'Post navigation', 'karyawp' ); ?></h2>
 			<div class="d-flex nav-links justify-content-between">
 				<?php
 				if ( get_previous_post_link() ) {
-					previous_post_link( '<span class="nav-previous">%link</span>', _x( '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/> </svg>&nbsp;%title', 'Previous post link', 'wpzaro' ) );
+					previous_post_link( '<span class="nav-previous">%link</span>', _x( '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/> </svg>&nbsp;%title', 'Previous post link', 'karyawp' ) );
 				}
 				if ( get_next_post_link() ) {
-					next_post_link( '<span class="nav-next">%link</span>', _x( '%title&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/> </svg>', 'Next post link', 'wpzaro' ) );
+					next_post_link( '<span class="nav-next">%link</span>', _x( '%title&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/> </svg>', 'Next post link', 'karyawp' ) );
 				}
 				?>
 			</div><!-- .nav-links -->
@@ -199,7 +197,7 @@ if ( ! function_exists( 'karyawp_link_pages' ) ) {
 		$args = apply_filters(
 			'karyawp_link_pages_args',
 			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wpzaro' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'karyawp' ),
 				'after'  => '</div>',
 			)
 		);
