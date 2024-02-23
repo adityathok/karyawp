8397e71fc92fcf91fff0a5377e99c172a7066f9a
adityathok <aditya0thok@gmail.com>
2024-02-23 21:57:54 +0700

    add archive

---
+++ archive.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * archive pages
+ * Description: The template for displaying archive pages.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package karyawp
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+
+    <main class="site-main" id="main">
+
+        <div class="row row-archive">
+
+            <?php
+            if ( have_posts() ) {
+                // Start the Loop.
+                $post_order = 1;
+                while ( have_posts() ) {
+                    the_post();
+                    get_template_part( 'partials/content/content', get_post_format() );
+                    $post_order++;
+                }
+            } else {
+                get_template_part( 'partials/content/none' );
+            }
+            ?>
+
+        </div>
+
+        <?php
+        // Display the pagination.
+        karyawp_pagination();
+        ?>
+
+    </main><!-- #main -->
+
+<?php
+get_footer();
